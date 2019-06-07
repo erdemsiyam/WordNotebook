97cab9bb2840ffe76fa41d30d6d519929213d1db
Erdem <erdemsiyam@gmail.com>
2019-06-07 21:04:07 +0300

    MainActivity changed to ListActivity(The page of showing word lists)

--- app/src/main/java/com/erdemsiyam/memorizeyourwords/ListActivity.java
+++ app/src/main/java/com/erdemsiyam/memorizeyourwords/ListActivity.java
@@ -1,14 +1,14 @@
 package com.erdemsiyam.memorizeyourwords;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_list);
     }
+
 }
