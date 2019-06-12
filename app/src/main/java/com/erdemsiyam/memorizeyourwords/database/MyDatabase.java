e6f6dc9ae81c608e955dde63b6e9c21090371f31
Erdem <erdemsiyam@gmail.com>
2019-06-12 14:19:31 +0300

    Added Confuse DAO Instance get function.

--- app/src/main/java/com/erdemsiyam/memorizeyourwords/database/MyDatabase.java
+++ app/src/main/java/com/erdemsiyam/memorizeyourwords/database/MyDatabase.java
@@ -5,16 +5,19 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import com.erdemsiyam.memorizeyourwords.entity.Category;
+import com.erdemsiyam.memorizeyourwords.entity.Confuse;
 import com.erdemsiyam.memorizeyourwords.entity.Word;
 import com.erdemsiyam.memorizeyourwords.repository.ICategoryDAO;
+import com.erdemsiyam.memorizeyourwords.repository.IConfuseDAO;
 import com.erdemsiyam.memorizeyourwords.repository.IWordDAO;
 
-@Database(entities = {Word.class, Category.class}, version = 1, exportSchema= false)
+@Database(entities = {Word.class, Category.class, Confuse.class}, version = 1, exportSchema= false)
 public abstract class MyDatabase extends RoomDatabase {
 
     private static final String DB_NAME = "MemorizeYourWords2.db";
     public abstract ICategoryDAO getCategoryDAO();
     public abstract IWordDAO getWordDAO();
+    public abstract IConfuseDAO getConfuseDAO();
 
     private static MyDatabase myDatabase;
     public static MyDatabase getMyDatabase(Context context) {
