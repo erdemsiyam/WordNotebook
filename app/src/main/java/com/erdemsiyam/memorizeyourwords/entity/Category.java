7e69410bacc4ac4f08f3e41b35ad6ac5525b7abc
Erdem <erdemsiyam@gmail.com>
2019-07-03 17:42:01 +0300

    line corrections

--- app/src/main/java/com/erdemsiyam/memorizeyourwords/entity/Category.java
+++ app/src/main/java/com/erdemsiyam/memorizeyourwords/entity/Category.java
@@ -15,35 +15,15 @@ public class Category {
     @Ignore
     private List<Word> words;
 
-    public Long getId() {
-        return id;
-    }
+    public Long getId() { return id; }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    public void setId(Long id) { this.id = id; }
 
-    public String getName() {
-        return name;
-    }
+    public String getName() { return name; }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    public void setName(String name) { this.name = name; }
 
-    public String getColor() {
-        return color;
-    }
+    public String getColor() { return color; }
 
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public List<Word> getWords() {
-        return words;
-    }
-
-    public void setWords(List<Word> words) {
-        this.words = words;
-    }
+    public void setColor(String color) { this.color = color; }
 }
