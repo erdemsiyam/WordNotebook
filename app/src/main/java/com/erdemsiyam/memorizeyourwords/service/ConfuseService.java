c983702271683b8e408e97fcfbca9bd0b75f0eb4
Erdem <erdemsiyam@gmail.com>
2019-07-03 17:56:23 +0300

    Needless things are removed. And Confuse object will be created with this static method "addConfuse",  and also "getAllConfuse", "getWrongsInsteadOfThisWord" methods added.

--- app/src/main/java/com/erdemsiyam/memorizeyourwords/service/ConfuseService.java
+++ app/src/main/java/com/erdemsiyam/memorizeyourwords/service/ConfuseService.java
@@ -8,10 +8,25 @@ import com.erdemsiyam.memorizeyourwords.entity.Word;
 import java.util.List;
 
 public class ConfuseService {
-    public static List<Confuse> getWrongsInsteadOfThisWord(Context context, Word word){
-        return MyDatabase.getMyDatabase(context).getConfuseDAO().getWrongsInsteadOfThisWord(word.getId());
+    public static void addConfuse(Context context, Long id, Long targetId){
+        Confuse confuse = MyDatabase.getMyDatabase(context).getConfuseDAO().getConfuse(id,targetId);
+        if(confuse != null){
+            MyDatabase.getMyDatabase(context).getConfuseDAO().increaseTimes(confuse.getId());
+        }else{
+            Confuse confuseNew = new Confuse();
+            confuseNew.setWordId(id);
+            confuseNew.setWrongWordId(targetId);
+            confuseNew.setTimes(1);
+            MyDatabase.getMyDatabase(context).getConfuseDAO().insertConfuse(confuseNew);
+        }
     }
-    public static List<Confuse> getThisWordInsteadOfOthers(Context context, Word word){
-        return MyDatabase.getMyDatabase(context).getConfuseDAO().getThisWordInsteadOfOthers(word.getId());
+    public static List<Confuse> getAllConfuse(Context context){
+        return MyDatabase.getMyDatabase(context).getConfuseDAO().getAllConfuse();
+    }
+    public static List<Confuse> getWrongsInsteadOfThisWord(Context context, Long wordId){
+        return MyDatabase.getMyDatabase(context).getConfuseDAO().getWrongsInsteadOfThisWord(wordId);
+    }
+    public static List<Confuse> getThisWordInsteadOfOthers(Context context, Long wordId){
+        return MyDatabase.getMyDatabase(context).getConfuseDAO().getThisWordInsteadOfOthers(wordId);
     }
 }
