c53051b6af8c9c6e4982712f5a592c56ba5b8f5c
Erdem <erdemsiyam@gmail.com>
2019-07-03 17:52:10 +0300

    getConfuse, increaseTimes, getAllConfuse, deleteWord Funcs Added.

--- app/src/main/java/com/erdemsiyam/memorizeyourwords/repository/IConfuseDAO.java
+++ app/src/main/java/com/erdemsiyam/memorizeyourwords/repository/IConfuseDAO.java
@@ -16,4 +16,12 @@ public interface IConfuseDAO {
     List<Confuse> getWrongsInsteadOfThisWord(Long id);
     @Query("SELECT * FROM Confuse WHERE wrong_word_id=:id")
     List<Confuse> getThisWordInsteadOfOthers(Long id);
+    @Query("SELECT * FROM Confuse WHERE id=:id AND wrong_word_id=:target_id")
+    Confuse getConfuse(Long id, Long target_id);
+    @Query("UPDATE Confuse SET times = times + 1 WHERE id =:id")
+    void increaseTimes(Long id);
+    @Query("SELECT * FROM Confuse")
+    List<Confuse> getAllConfuse();
+    @Query("DELETE FROM Confuse WHERE word_id=:id OR wrong_word_id=:id")
+    void deleteWord(Long id);
 }
