78cb315396264d4c3b1881488e9e1f140ed575ff
Erdem <erdemsiyam@gmail.com>
2019-07-03 17:54:46 +0300

    Needless things are removed.

--- app/src/main/java/com/erdemsiyam/memorizeyourwords/service/CategoryService.java
+++ app/src/main/java/com/erdemsiyam/memorizeyourwords/service/CategoryService.java
@@ -7,44 +7,25 @@ import com.erdemsiyam.memorizeyourwords.entity.Category;
 import java.util.List;
 
 public final class CategoryService {
-    private static List<Category> categories;
 
     public static List<Category> getCategories(Context context){
-        if(categories == null)
-            categories = MyDatabase.getMyDatabase(context).getCategoryDAO().getAllCategory();
-        return categories;
+        return MyDatabase.getMyDatabase(context).getCategoryDAO().getAllCategory();
+    }
+    public static Category getCategoryById(Context context, Long id){
+        return MyDatabase.getMyDatabase(context).getCategoryDAO().getCategoryById(id);
+    }
+    public static Category addCategory(Context context,String name,String color){
+        Category category = new Category();
+        category.setName(name);
+        category.setColor(color);
+        category.setId(MyDatabase.getMyDatabase(context).getCategoryDAO().insertCategory(category));
+        return category;
     }
     public static void updateCategory(Context context, Category updatedCategory){
         MyDatabase.getMyDatabase(context).getCategoryDAO().updateCategory(updatedCategory);
-        Category existsCategory = findCategoryInStaticList(updatedCategory);
-        if(existsCategory == null) throw new RuntimeException("Category Not Found.");
-        existsCategory.setColor(updatedCategory.getColor());
-        existsCategory.setName(updatedCategory.getName());
     }
     public static void deleteCategory(Context context, Category willRemoveCategory){
-        Category existsCategory = findCategoryInStaticList(willRemoveCategory);
-        if(existsCategory == null){throw new RuntimeException("Category Not Found.");} // actually not need we already trying to delete this.
-        categories.remove(existsCategory);
         MyDatabase.getMyDatabase(context).getCategoryDAO().deleteCategory(willRemoveCategory);
     }
-    public static void addCategory(Context context, Category category){
-        category.setId(MyDatabase.getMyDatabase(context).getCategoryDAO().insertCategory(category));
-        categories.add(category);
-    }
-    // buna gerek var mı
-    private static Category findCategoryInStaticList(Category willSearchCategory){
-        Category existsCategory = null;
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
-            existsCategory = categories.stream().filter(x -> willSearchCategory.getId().equals(x.getId())).findAny().orElse(null);
-        }
-        else {
-            for (Category c : categories) {
-                if(c.getId().equals(willSearchCategory.getId())){
-                    existsCategory = c;
-                    break;
-                }
-            }
-        }
-        return existsCategory;
-    }
+
 }
