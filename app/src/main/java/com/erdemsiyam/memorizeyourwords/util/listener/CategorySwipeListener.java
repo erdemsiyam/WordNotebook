573dae523527b7fb233dc987e83703001df01fcd
Erdem <erdemsiyam@gmail.com>
2019-07-19 22:00:40 +0300

    Click and Long Click codded.

--- app/src/main/java/com/erdemsiyam/memorizeyourwords/util/listener/CategorySwipeListener.java
+++ app/src/main/java/com/erdemsiyam/memorizeyourwords/util/listener/CategorySwipeListener.java
@@ -1,27 +1,55 @@
 package com.erdemsiyam.memorizeyourwords.util.listener;
 
+import android.util.Log;
+
 import com.erdemsiyam.memorizeyourwords.entity.Category;
+import com.erdemsiyam.memorizeyourwords.util.adapter.CategoryRecyclerViewAdapter;
 
 import co.dift.ui.SwipeToAction;
 
 public class CategorySwipeListener implements SwipeToAction.SwipeListener<Category> {
+    private CategoryRecyclerViewAdapter adapter;
+    public CategorySwipeListener(CategoryRecyclerViewAdapter adapter){
+        this.adapter = adapter;
+    }
     @Override
     public boolean swipeLeft(Category itemData) {
-        return false;
+        return true;
     }
 
     @Override
     public boolean swipeRight(Category itemData) {
-        return false;
+        return true;
     }
 
     @Override
-    public void onClick(Category itemData) {
-
+    public void onClick(Category category) {
+        if(adapter.getSelectedCategoryCount() > 0){ // if there is exist selected item, We got it Select Action Active.
+            Log.i("erdem","kategori uzun tiklandi");
+            selectingCategory(category);
+        }
+        else{
+            // açma işlemi
+            Log.i("erdem","kategori tiklandi aç");
+        }
     }
 
     @Override
-    public void onLongClick(Category itemData) {
+    public void onLongClick(Category category) {
+        Log.i("erdem","kategori uzun tiklandi");
+        selectingCategory(category);
+    }
 
+    private void selectingCategory(Category category){
+        adapter.displaySelectingActionModeIfNotExist(); // if action mod null : then fill it and start.
+        adapter.toggleSelection(category); // switch select or non-select.
+
+        int selectedCount = adapter.getSelectedCategoryCount();
+        if( selectedCount == 0){ // if all selects are removed, then Action Mode Will Closed.
+            adapter.selectingCategoriesActionMode.finish();
+        }else{ // if there is selecting items exists, write these count to the title.
+            adapter.selectingCategoriesActionMode.setTitle(selectedCount + " Seçildi");
+            adapter.selectingCategoriesActionMode.invalidate();
+        }
     }
 }
