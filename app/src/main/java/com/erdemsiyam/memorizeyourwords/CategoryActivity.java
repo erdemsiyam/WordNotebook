6016db127e00e43fa5df5c176bd18caf81b15b73
Erdem <erdemsiyam@gmail.com>
2019-08-21 16:53:24 +0300

    Added, going to exam with AlertDialog (selecting categories and which words)

--- app/src/main/java/com/erdemsiyam/memorizeyourwords/CategoryActivity.java
+++ app/src/main/java/com/erdemsiyam/memorizeyourwords/CategoryActivity.java
@@ -1,7 +1,9 @@
 package com.erdemsiyam.memorizeyourwords;
 
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.ActionMode;
 import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
@@ -10,29 +12,36 @@ import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.inputmethod.EditorInfo;
-import android.widget.Button;
 
 import com.erdemsiyam.memorizeyourwords.entity.Category;
+import com.erdemsiyam.memorizeyourwords.service.CategoryService;
 import com.erdemsiyam.memorizeyourwords.util.adapter.CategoryRecyclerViewAdapter;
-import com.erdemsiyam.memorizeyourwords.util.listener.CategoryAddFABListener;
-import com.erdemsiyam.memorizeyourwords.util.listener.CategorySearchListener;
-import com.erdemsiyam.memorizeyourwords.util.listener.CategorySwipeListener;
+import com.erdemsiyam.memorizeyourwords.util.listener.category.CategoryAddFABListener;
+import com.erdemsiyam.memorizeyourwords.util.listener.category.CategorySearchListener;
+import com.erdemsiyam.memorizeyourwords.util.listener.category.CategorySelectActionModeCallBack;
+import com.erdemsiyam.memorizeyourwords.util.listener.category.CategorySwipeListener;
 import com.erdemsiyam.memorizeyourwords.util.listener.NavigationItemSelectListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import co.dift.ui.SwipeToAction;
 
 public class CategoryActivity extends AppCompatActivity {
+
+    public static final String INTENT_CATEGORY_ID = "category_id";
+    public static final String INTENT_EXAM_SELECT_INDEX = "examSelectIndex";
+    public static final String INTENT_SELECTED_CATEGORY_IDS = "selectedCategoryIds";
+    private int examSelectIndex=-1;
+
 
     private RecyclerView recyclerView; // our recycler of category list
     private CategoryRecyclerViewAdapter adapter;
@@ -49,7 +58,6 @@ public class CategoryActivity extends AppCompatActivity {
         initComponents(); // creating objects.
         loadData(); // test methods
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // category search icon adding to tool bar
@@ -62,7 +70,6 @@ public class CategoryActivity extends AppCompatActivity {
 
         return super.onCreateOptionsMenu(menu);
     }
-
     @Override
     public void onBackPressed() {
         if(drawerLayout.isDrawerOpen(GravityCompat.START)) // if navigation menu opened (THE LEFT MENU)
@@ -70,6 +77,7 @@ public class CategoryActivity extends AppCompatActivity {
         else
             super.onBackPressed();
     }
+
 
     private void initComponents(){
         //our custimozed tool bar include
@@ -80,7 +88,7 @@ public class CategoryActivity extends AppCompatActivity {
         drawerLayout = findViewById(R.id.category_activity);
 
         //navigation menu include
-        NavigationView navigationView = findViewById(R.id.navView);
+        NavigationView navigationView = findViewById(R.id.navViewCategory);
         navigationView.setNavigationItemSelectedListener(new NavigationItemSelectListener(this));
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolBar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
@@ -92,31 +100,7 @@ public class CategoryActivity extends AppCompatActivity {
 
     }
     private void loadData(){
-        Category category1 = new Category();
-            category1.setId(1L);
-            category1.setName("Günler");
-            category1.setColor("");
-            category1.setAlarm(0l);
-        Category category2 = new Category();
-            category2.setId(2L);
-            category2.setName("Aylar");
-            category2.setColor("");
-            category2.setAlarm(0l);
-        Category category3 = new Category();
-            category3.setId(3L);
-            category3.setName("Hava Durumları");
-            category3.setColor("");
-            category3.setAlarm(0l);
-        Category category4 = new Category();
-            category4.setId(4L);
-            category4.setName("Ev Eşyaları");
-            category4.setColor("");
-            category4.setAlarm(0l);
-        List<Category> categories = new ArrayList<>();
-        categories.add(category1);
-        categories.add(category2);
-        categories.add(category3);
-        categories.add(category4);
+        List<Category> categories = CategoryService.getCategories(this);
 
         final LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         layoutManager.setOrientation(RecyclerView.VERTICAL);
@@ -125,7 +109,7 @@ public class CategoryActivity extends AppCompatActivity {
         adapter = new CategoryRecyclerViewAdapter(this,categories);
         recyclerView.setAdapter(adapter); // the category list sended.
         recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
-        swipeToAction = new SwipeToAction(recyclerView, new CategorySwipeListener(adapter));
+        swipeToAction = new SwipeToAction(recyclerView, new CategorySwipeListener(this,adapter));
     }
 
     public void deleteCategories(){
@@ -134,6 +118,34 @@ public class CategoryActivity extends AppCompatActivity {
             adapter.removeData(positions.get(i));
         }
         adapter.notifyDataSetChanged();
+    }
+
+    public AlertDialog examWordsSelecting(ActionMode mode){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Sınav Seçiniz");
+        String[] options = {"Hepsi","Öğrenilenler","Yıldızlananlar","Öğrenilmeyenler"};
+        builder.setSingleChoiceItems(options, 0, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                examSelectIndex = i;
+            }
+        });
+        builder.setPositiveButton("Başla", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(getApplicationContext(), ExamActivity.class);
+                intent.putExtra(INTENT_EXAM_SELECT_INDEX,examSelectIndex);
+                intent.putExtra(INTENT_SELECTED_CATEGORY_IDS,getAdapter().getSelectedCategoryIds());
+                mode.finish();
+                startActivity(intent);
+            }
+        });
+        builder.setNegativeButton("İptal", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+        return builder.create();
     }
 
 
