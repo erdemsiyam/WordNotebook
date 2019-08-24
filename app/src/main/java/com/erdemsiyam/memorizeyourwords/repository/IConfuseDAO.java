b03c6e9582ed92f3cbea407aeb426ce4e8ee2f30
Erdem <erdemsiyam@gmail.com>
2019-08-24 13:53:11 +0300

    Fixed the names of 2 functions Added "DeleteAllConfuse" Function.

--- app/src/main/java/com/erdemsiyam/memorizeyourwords/repository/IConfuseDAO.java
+++ app/src/main/java/com/erdemsiyam/memorizeyourwords/repository/IConfuseDAO.java
@@ -13,15 +13,17 @@ public interface IConfuseDAO {
     @Delete
     void deleteConfuse(Confuse confuse);
     @Query("SELECT * FROM Confuse WHERE word_id=:id")
-    List<Confuse> getWrongsInsteadOfThisWord(Long id);
+    List<Confuse> getConfusesWordsByFirstId(Long id);
     @Query("SELECT * FROM Confuse WHERE wrong_word_id=:id")
-    List<Confuse> getThisWordInsteadOfOthers(Long id);
-    @Query("SELECT * FROM Confuse WHERE id=:id AND wrong_word_id=:target_id")
+    List<Confuse> getConfusesWordsBySecondId(Long id);
+    @Query("SELECT * FROM Confuse WHERE word_id=:id AND wrong_word_id=:target_id")
     Confuse getConfuse(Long id, Long target_id);
     @Query("UPDATE Confuse SET times = times + 1 WHERE id =:id")
     void increaseTimes(Long id);
     @Query("SELECT * FROM Confuse")
     List<Confuse> getAllConfuse();
     @Query("DELETE FROM Confuse WHERE word_id=:id OR wrong_word_id=:id")
-    void deleteWord(Long id);
+    void deleteConfusesByWordId(Long id);
+    @Query("DELETE FROM Confuse")
+    void deleteAllConfuse();
 }
