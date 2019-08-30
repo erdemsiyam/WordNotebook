f6f8be82bfaafbba6834cd425fcd3f7b8562f917
Erdem <erdemsiyam@gmail.com>
2019-08-31 00:12:50 +0300

    Constants are removed because we will use string resource.

--- app/src/main/java/com/erdemsiyam/memorizeyourwords/exception/MyException.java
+++ app/src/main/java/com/erdemsiyam/memorizeyourwords/exception/MyException.java
@@ -2,7 +2,4 @@ package com.erdemsiyam.memorizeyourwords.exception;
 
 public class MyException extends RuntimeException {
     public MyException(String message) {super(message);}
-
-    public final static String NO_CONTENT = "Yazı Girmelisiniz.";
-    public final static String CONTENT_LIMIT_EXCEEDED = "30 karakter ve aşağısı olmalı.";
 }
