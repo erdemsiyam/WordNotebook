1e303cdbf12f28a8737057bf78584a3b30982a5a
Erdem <erdemsiyam@gmail.com>
2019-09-08 23:12:15 +0300

    Class name changed. For better understanding.

--- app/src/main/java/com/erdemsiyam/memorizeyourwords/util/WordGroupType.java
+++ app/src/main/java/com/erdemsiyam/memorizeyourwords/util/WordGroupType.java
@@ -6,7 +6,7 @@ import androidx.annotation.StringRes;
 
 import com.erdemsiyam.memorizeyourwords.R;
 
-public enum ExamWordType {
+public enum WordGroupType {
     All(0,R.string.type_exam_word_all),
     Learned(1,R.string.type_exam_word_learned),
     Marked(2,R.string.type_exam_word_marked),
@@ -16,16 +16,16 @@ public enum ExamWordType {
     @StringRes
     public int value; // It is a string resource. Equals multiple language texts.
 
-    ExamWordType(int key,@StringRes int value){this.key = key;this.value = value;}
+    WordGroupType(int key, @StringRes int value){this.key = key;this.value = value;}
     public static String[] getValuesAsStringArray(Context context){
-        String[] values = new String[ExamWordType.values().length];
-        for(int i = 0; i < ExamWordType.values().length; i++){
-            values[i] = context.getResources().getString(ExamWordType.values()[i].value);
+        String[] values = new String[WordGroupType.values().length];
+        for(int i = 0; i < WordGroupType.values().length; i++){
+            values[i] = context.getResources().getString(WordGroupType.values()[i].value);
         }
         return values;
     }
-    public static ExamWordType getTypeByKey(int key){
-        for (ExamWordType type : values()) {
+    public static WordGroupType getTypeByKey(int key){
+        for (WordGroupType type : values()) {
             if (type.key == key)
                 return type;
         }
