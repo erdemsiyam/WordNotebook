208668d0154a1aaab13b9937e15cc817a2b9478a
Erdem <erdemsiyam@gmail.com>
2019-09-08 23:07:56 +0300

    NotificationWord model introduce to DB.

--- app/src/main/java/com/erdemsiyam/memorizeyourwords/database/MyDatabase.java
+++ app/src/main/java/com/erdemsiyam/memorizeyourwords/database/MyDatabase.java
@@ -6,18 +6,21 @@ import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import com.erdemsiyam.memorizeyourwords.entity.Category;
 import com.erdemsiyam.memorizeyourwords.entity.Confuse;
+import com.erdemsiyam.memorizeyourwords.entity.NotificationWord;
 import com.erdemsiyam.memorizeyourwords.entity.Word;
 import com.erdemsiyam.memorizeyourwords.repository.ICategoryDAO;
 import com.erdemsiyam.memorizeyourwords.repository.IConfuseDAO;
+import com.erdemsiyam.memorizeyourwords.repository.INotificationWordDAO;
 import com.erdemsiyam.memorizeyourwords.repository.IWordDAO;
 
-@Database(entities = {Word.class, Category.class, Confuse.class}, version = 1, exportSchema= false)
+@Database(entities = {Word.class, Category.class, Confuse.class, NotificationWord.class}, version = 1, exportSchema= false)
 public abstract class MyDatabase extends RoomDatabase {
 
     private static final String DB_NAME = "WordNotebook.db";
     public abstract ICategoryDAO getCategoryDAO();
     public abstract IWordDAO getWordDAO();
     public abstract IConfuseDAO getConfuseDAO();
+    public abstract INotificationWordDAO getNotificationWordDAO();
 
     private static MyDatabase myDatabase;
     public static MyDatabase getMyDatabase(Context context) {
