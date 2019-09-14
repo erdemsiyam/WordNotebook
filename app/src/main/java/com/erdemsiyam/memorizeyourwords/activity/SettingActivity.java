b18beabc626b3379cc83be83ed47661c39cdcc6f
Erdem <erdemsiyam@gmail.com>
2019-09-14 07:50:09 +0300

    General "Notification" Settings added like (Sound,Vibration,Heads-Up). SharedPrefences's "commit()" codes replaced with "apply()". Added A flag "NotificationIsChange" to realise is there any changes at notification. And added "channel_id" to shared preferences ; all notification uses this.

--- app/src/main/java/com/erdemsiyam/memorizeyourwords/activity/SettingActivity.java
+++ app/src/main/java/com/erdemsiyam/memorizeyourwords/activity/SettingActivity.java
@@ -3,12 +3,16 @@ import android.app.Dialog;
 import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.NumberPicker;
+import android.widget.Switch;
 import android.widget.TimePicker;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatImageButton;
 import androidx.appcompat.widget.AppCompatTextView;
@@ -27,16 +31,27 @@ public class SettingActivity extends AppCompatActivity {
     public static final String  WORD_NOTIFICATION_START_TIME_MINUTE = "word_notification_start_time_minute";
     public static final String  WORD_NOTIFICATION_END_TIME_HOUR = "word_notification_end_time_hour";
     public static final String  WORD_NOTIFICATION_END_TIME_MINUTE = "word_notification_end_time_minute";
+    public static final String  NOTIFICATION_SOUND = "notification_sound";
+    public static final String  NOTIFICATION_VIBRATE = "notification_vibrate";
+    public static final String  NOTIFICATION_HEADS_UP = "notification_heads_up";
+    public static final String  NOTIFICATION_IS_CHANGE = "notification_is_change";
+    public static final String  NOTIFICATION_GENERAL_CHANNEL_ID = "notification_general_channel_id";
     public static final String  FONT = "font";
 
     /* UI Components. */
     private LinearLayout         lytWordNotificationPeriod;
     private LinearLayout         lytWordNotificationStartTime;
     private LinearLayout         lytWordNotificationEndTime;
+    private LinearLayout         lytNotificationSound;
+    private LinearLayout         lytNotificationVibrate;
+    private LinearLayout         lytNotificationHeadsUp;
     private LinearLayout         lytFont;
     private AppCompatTextView    txtWordNotificationPeriodValue;
     private AppCompatTextView    txtWordNotificationStartTimeValue;
     private AppCompatTextView    txtWordNotificationEndTimeValue;
+    private Switch               swNotificationSoundValue;
+    private Switch               swNotificationVibrateValue;
+    private Switch               swNotificationHeadsUpValue;
     private AppCompatTextView    txtFontValue;
     private AppCompatImageButton btnBackToCategoryFromSetting; // Back button to "CategoryActivity".
 
@@ -58,10 +73,19 @@ public class SettingActivity extends AppCompatActivity {
         lytWordNotificationPeriod = findViewById(R.id.lytWordNotificationPeriod);
         lytWordNotificationStartTime = findViewById(R.id.lytWordNotificationStartTime);
         lytWordNotificationEndTime = findViewById(R.id.lytWordNotificationEndTime);
+        lytNotificationSound = findViewById(R.id.lytNotificationSound);
+        lytNotificationVibrate = findViewById(R.id.lytNotificationVibrate);
+        lytNotificationHeadsUp = findViewById(R.id.lytNotificationHeadsUp);
         lytFont = findViewById(R.id.lytFont);
         txtWordNotificationPeriodValue = findViewById(R.id.txtWordNotificationPeriodValue);
         txtWordNotificationStartTimeValue = findViewById(R.id.txtWordNotificationStartTimeValue);
         txtWordNotificationEndTimeValue = findViewById(R.id.txtWordNotificationEndTimeValue);
+        swNotificationSoundValue = findViewById(R.id.swNotificationSoundValue);
+        swNotificationVibrateValue = findViewById(R.id.swNotificationVibrateValue);
+        swNotificationHeadsUpValue = findViewById(R.id.swNotificationHeadsUpValue);
+        swNotificationSoundValue.setClickable(false);
+        swNotificationVibrateValue.setClickable(false);
+        swNotificationHeadsUpValue.setClickable(false);
         txtFontValue = findViewById(R.id.txtFontValue);
         btnBackToCategoryFromSetting = findViewById(R.id.btnBackToCategoryFromSetting);
     }
@@ -77,6 +101,9 @@ public class SettingActivity extends AppCompatActivity {
         int endHourInt = sharedPreferences.getInt(WORD_NOTIFICATION_END_TIME_HOUR,23);
         int endMinuteInt = sharedPreferences.getInt(WORD_NOTIFICATION_END_TIME_MINUTE,59);
         txtWordNotificationEndTimeValue.setText(((endHourInt<10)?"0"+endHourInt:""+endHourInt)+" : "+((endMinuteInt<10)?"0"+endMinuteInt:""+endMinuteInt));
+        swNotificationSoundValue.setChecked(sharedPreferences.getBoolean(NOTIFICATION_SOUND,true));
+        swNotificationVibrateValue.setChecked(sharedPreferences.getBoolean(NOTIFICATION_VIBRATE,true));
+        swNotificationHeadsUpValue.setChecked(sharedPreferences.getBoolean(NOTIFICATION_HEADS_UP,true));
         txtFontValue.setText((12 + 2*sharedPreferences.getInt(FONT,2))+" sp");
 
         /* Listeners for settings. */
@@ -95,7 +122,7 @@ public class SettingActivity extends AppCompatActivity {
                 btnSettingNumberPickAccept.setOnClickListener(new View.OnClickListener(){
                     @Override
                     public void onClick(View v) {
-                        sharedPreferences.edit().putInt(WORD_NOTIFICATION_PERIOD,np.getValue()).commit(); // WordNotification "CycleTime" changed.
+                        sharedPreferences.edit().putInt(WORD_NOTIFICATION_PERIOD,np.getValue()).apply(); // WordNotification "CycleTime" changed.
                         txtWordNotificationPeriodValue.setText(np.getValue() + " " + getResources().getString(R.string.minute)); // UI refreshed.
                         restartWordNotificationService(); // Restarting "WordNotificationService".
                         d.dismiss(); // Dialog closed.
@@ -114,7 +141,7 @@ public class SettingActivity extends AppCompatActivity {
                         sharedPreferences.edit()
                                 .putInt(WORD_NOTIFICATION_START_TIME_HOUR,hourOfDay)
                                 .putInt(WORD_NOTIFICATION_START_TIME_MINUTE,minute)
-                                .commit(); // Start time value changed.
+                                .apply(); // Start time value changed.
                         txtWordNotificationStartTimeValue.setText(((hourOfDay<10)?"0"+hourOfDay:""+hourOfDay)+" : "+((minute<10)?"0"+minute:""+minute)); // UI refreshed.
                         restartWordNotificationService(); // Restarting "WordNotificationService".
                     }
@@ -134,7 +161,7 @@ public class SettingActivity extends AppCompatActivity {
                         sharedPreferences.edit()
                                 .putInt(WORD_NOTIFICATION_END_TIME_HOUR,hourOfDay)
                                 .putInt(WORD_NOTIFICATION_END_TIME_MINUTE,minute)
-                                .commit(); // End time value changed.
+                                .apply(); // End time value changed.
                         txtWordNotificationEndTimeValue.setText(((hourOfDay<10)?"0"+hourOfDay:""+hourOfDay)+" : "+((minute<10)?"0"+minute:""+minute)); // UI refreshed.
                         restartWordNotificationService(); // Restarting "WordNotificationService".
                     }
@@ -142,6 +169,33 @@ public class SettingActivity extends AppCompatActivity {
                 TimePickerDialog timePickerDialog = new TimePickerDialog(SettingActivity.this, new TimeHandler(), 23, 59, true);
                 timePickerDialog.setTitle(R.string.setting_word_notification_alert_title_set_time);
                 timePickerDialog.show();
+            }
+        });
+        lytNotificationSound.setOnClickListener(v -> {
+            boolean newStatus = !swNotificationSoundValue.isChecked();
+            swNotificationSoundValue.setChecked(newStatus);
+            sharedPreferences.edit()
+                    .putBoolean(NOTIFICATION_SOUND,newStatus)
+                    .putBoolean(NOTIFICATION_IS_CHANGE,true)
+                    .apply();
+        });
+        lytNotificationVibrate.setOnClickListener(v -> {
+            boolean newStatus = !swNotificationVibrateValue.isChecked();
+            swNotificationVibrateValue.setChecked(newStatus);
+            sharedPreferences.edit()
+                    .putBoolean(NOTIFICATION_VIBRATE,newStatus)
+                    .putBoolean(NOTIFICATION_IS_CHANGE,true)
+                    .apply();
+        });
+        lytNotificationHeadsUp.setOnClickListener(v -> {
+            boolean newStatus = !swNotificationHeadsUpValue.isChecked();
+            swNotificationHeadsUpValue.setChecked(newStatus);
+            sharedPreferences.edit()
+                    .putBoolean(NOTIFICATION_HEADS_UP,newStatus)
+                    .putBoolean(NOTIFICATION_IS_CHANGE,true)
+                    .apply();
+            if(android.os.Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT){
+                Toast.makeText(SettingActivity.this,"Error : This feature is for Android 5.0 and above.",Toast.LENGTH_LONG).show();
             }
         });
         lytFont.setOnClickListener(new View.OnClickListener(){
@@ -160,7 +214,7 @@ public class SettingActivity extends AppCompatActivity {
                 btnSettingNumberPickAccept.setOnClickListener(new View.OnClickListener(){
                     @Override
                     public void onClick(View v) {
-                        sharedPreferences.edit().putInt(FONT,np.getValue()).commit(); // "FontSize" changed.
+                        sharedPreferences.edit().putInt(FONT,np.getValue()).apply(); // "FontSize" changed.
                         txtFontValue.setText((12 + 2*np.getValue()) + " sp"); // UI refreshed.
                         d.dismiss(); // Dialog closed.
                     }
