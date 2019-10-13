515781a4e180014743ef350966372708af367f79
Erdem <erdemsiyam@gmail.com>
2019-10-13 22:30:14 +0300

    word group type ids changed to fix listing bug.

--- app/src/main/java/com/erdemsiyam/memorizeyourwords/util/WordGroupType.java
+++ app/src/main/java/com/erdemsiyam/memorizeyourwords/util/WordGroupType.java
@@ -6,8 +6,8 @@ import com.erdemsiyam.memorizeyourwords.R;
 
 public enum WordGroupType {
     All(0,R.string.type_exam_word_all),
-    Marked(2,R.string.type_exam_word_marked),
-    Learned(1,R.string.type_exam_word_learned),
+    Marked(1,R.string.type_exam_word_marked),
+    Learned(2,R.string.type_exam_word_learned),
     NotLearned(3,R.string.type_exam_word_notlearned);
 
     public int key; // It is an identity
